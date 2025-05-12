1e81bd018516b619f8d4e4fddb3bea66f1f1dc4a
Katerina Fajmanova <k.moudra@seznam.cz>
2025-05-12 07:19:12 +0200

    Update create_adversarial_pattern.py

--- src/create_adversarial_pattern.py
+++ src/create_adversarial_pattern.py
@@ -18,11 +18,12 @@ def create_adversarial_pattern(model, input_image):
         tape.watch(input_image)  # Explicitly watch the input
         prediction = model(input_image)
         loss = tf.keras.losses.sparse_categorical_crossentropy(input_label, prediction)
-
+        print("after loss")
     # Get the gradients of the loss with respect to the input image
     gradient = tape.gradient(loss, input_image)
-
+    print("after gradient")
     # Get the sign of the gradients to create the adversarial pattern
     signed_grad = tf.sign(gradient)
+    print("after signum")
 
     return signed_grad
